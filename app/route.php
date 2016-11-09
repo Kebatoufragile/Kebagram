fd580eb2ee191e5c013d2c5f1fb632eb45b6cca5
Kixot <seb.kixot@gmail.com>
2016-11-09 11:12:53 +0100

    Handling route errors

--- app/route.php
+++ app/route.php
@@ -8,11 +8,15 @@ $app->get('/', 'App\Controllers\HomeController:dispatch')->setName('accueil');
 
 $app->post('/login', 'App\Controllers\LoginController:dispatch')->setName('login');
 
+$app->get('/login', 'App\Controllers\HomeController:dispatch')->setName('login');
+
 $app->get('/profile', 'App\Controllers\ProfileController:dispatch')->setName('profile');
 
 $app->get('/addpic', 'App\Controllers\PictureController:dispatch')->setName('addpic');
 
 $app->post('/upload', 'App\Controllers\PictureController:upload')->setName('upload');
+
+$app->get('/upload', 'App\Controllers\HomeController:dispatch')->setName('upload');
 
 $app->get('/signup', 'App\Controllers\InscriptionController:dispatch')->setName('signup');
 
