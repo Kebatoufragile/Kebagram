d3b774a8fb0d5d52dbbf832f277b0c6e2a879ee2
Kixot <seb.kixot@gmail.com>
2016-11-11 17:26:33 +0100

    [DONE] modify profile picture

--- app/route.php
+++ app/route.php
@@ -30,6 +30,8 @@ $app->post('/modifyPassword', 'App\Controllers\ProfileController:modifyPassword'
 
 $app->post('/search', 'App\Controllers\ResearchController:dispatch')->setName('search');
 
+$app->post('/modifyPicture', 'App\Controllers\ProfileController:modifyPicture')->setName('modifyPicture');
+
 
 // routes for error handling
 $app->get('/login', 'App\Controllers\HomeController:dispatch')->setName('login');
@@ -41,3 +43,5 @@ $app->get('/modify', 'App\Controllers\HomeController:dispatch')->setName('modify
 $app->get('/modifyPassword', 'App\Controllers\HomeController:dispatch')->setName('modifyPassword');
 
 $app->get('/search', 'App\Controllers\HomeController:dispatch')->setName('search');
+
+$app->get('/modifyPicture', 'App\Controllers\HomeController:dispatch')->setName('modifyPicture');
